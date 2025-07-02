6149b390dc18c9213e738fdc1a41ac76486581ba
xuanmanh-2110 <206378505+xuanmanh-2110@users.noreply.github.com>
2025-07-02 15:10:41 +0700

    Refactor Vite configuration to support dynamic base path based on command

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
@@ -10,7 +10,7 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
-    base: '/build/',
+    base: command === 'build' ? '/build/' : '/',
     build: {
         manifest: true,
         outDir: 'public/build',
@@ -25,4 +25,4 @@ export default defineConfig({
     server: {
         https: true,
     },
-});
+}));
